406ea5c482b8f29d3e6c5c0120c91dfca66771d4
Akhtem <akhtem.khasyanov-ext@varjo.com>
2021-08-24 01:08:16 +0300

    updates

--- react/index.js
+++ react/index.js
@@ -1,29 +1,43 @@
 const baseRules = require('../base/index')
 
 module.exports = {
-    extends: [
-        "plugin:jsx-a11y/recommended",
-		"airbnb",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/recommended",
-		"plugin:unicorn/recommended",
-		"plugin:jsdoc/recommended",
-		"plugin:react-hooks/recommended"
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true
-        }
-    },
     env: {
         es6: true,
         browser: true,
         node: true,
         jest: true
+    },
+    extends: [
+        "airbnb-typescript",
+        "plugin:jest/recommended",
+        "plugin:unicorn/recommended",
+        "plugin:jsdoc/recommended",
+        "plugin:jsx-a11y/recommended",
+        "plugin:import/recommended",
+        "plugin:import/typescript",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        "@typescript-eslint"
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        },
+        project: "./tsconfig.json"
+    },
+    settings: {
+        "import/resolver": {
+            typescript: {
+                extensions: [".js", ".ts", ".tsx", "jsx"]
+            }
+        }
     },
     rules: {
         ...baseRules,
